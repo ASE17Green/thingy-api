00f0110a6f8a8e16aa23d62e94800f963a2e72ed
gareon19 <alain.kolp@hotmail.com>
2017-12-15 13:51:18 +0100

    adjusted delivery end location radius

--- models/thingy.js
+++ models/thingy.js
@@ -128,8 +128,8 @@ module.exports.checkLocation = function(thingy, callback){
   UserThingy.getUserThingyByID(thingy.thingyID, function (err, userThingy) {
     if (err) return next(err);
     var coef = 0.000089;
-    if(thingy.latitude < userThingy.endLatitude + coef/5
-       && thingy.latitude > userThingy.endLatitude - coef/5
+    if(thingy.latitude < userThingy.endLatitude + coef*2
+       && thingy.latitude > userThingy.endLatitude - coef*2
        && thingy.longitude < userThingy.endLongitude + coef/Math.cos(thingy.latitude*0.018)
        && thingy.longitude > userThingy.endLongitude - coef/Math.cos(thingy.latitude*0.018)
        && !userThingy.packageArrivedMessageSent){
